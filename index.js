e638512175d2c32c57770e676403c7150f62e813
Bennett Marcelo <xxleben85@yahoo.com>
2019-01-26 09:38:01 +0800

    finished basic requirement

--- index.js
+++ index.js
@@ -1,4 +1,8 @@
 $(document).ready(function() {
+
+  var timeLeft = 10;
+  var score = 0;
+
   var randomNum = function(num) {
   	return Math.ceil(Math.random() * num);
   };
@@ -14,17 +18,56 @@ $(document).ready(function() {
   	return problem;
   };
 
-  var currentEquation = mathProblem();
+  var currentEquation = '';
 
-  $('#calculation').text(currentEquation.equation);
+  var newEquation = function() {
+    currentEquation = mathProblem();
+    $('#calculation').text(currentEquation.equation);
+  };
+
+  var updateTimeLeft = function(amount) {
+    timeLeft += amount;
+    $('#time-left').text(timeLeft);
+  }
 
   var checkAnswer = function(userAnswer, answer) {
-    console.log(userAnswer === answer);
+    if (userAnswer === answer) {
+      newEquation();
+      $('#user-input').val('');
+      updateTimeLeft(+1);
+      updateScore(+1);
+    }
   };
 
   $('#user-input').on('keyup', function() {
+    startGame();
     checkAnswer(Number($(this).val()), currentEquation.answer)
   });
 
+  var interval;
 
+  var startGame = function() {
+    if (!interval) {
+      if (timeLeft === 0) {
+        updateTimeLeft(10);
+        $('#high-score').text(score);
+        updateScore(-score);
+      }
+      interval = setInterval(function() {
+        updateTimeLeft(-1);
+        if (timeLeft === 0) {
+          clearInterval(interval);
+          interval = undefined;
+          $('#user-input').val('');
+        }
+      }, 1000)
+    };
+  };
+
+  var updateScore = function(amount) {
+    score += amount;
+    $('#current-score').text(score);
+  }
+
+  newEquation();
 });
